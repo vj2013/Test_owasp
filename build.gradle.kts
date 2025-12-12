7d81eaedd7d5e922f28bef3f2f63f0a36f7f9072
jordan Vicaña <jvicanaa@gmail.com>
2025-12-12 15:43:33 -0500

    Update Jenkins pipeline

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,12 +33,25 @@ configurations.all {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("io.projectreactor.netty:reactor-netty-http:1.2.12")
 }
 
 dependencyManagement {
     imports {
         mavenBom("io.netty:netty-bom:${property("netty.version")}")
+    }
+}
+
+configurations.all {
+    resolutionStrategy {
+        // Asegúrate de usar la versión correcta de Netty que contiene los arreglos
+        val nettyVersion = property("netty.version") as String
+
+        // Forzar las dependencias base de Netty a la versión del BOM
+        // Estos son los componentes base que tienen las vulnerabilidades reportadas
+        force("io.netty:netty-codec-http:$nettyVersion")
+        force("io.netty:netty-handler:$nettyVersion")
+        force("io.netty:netty-buffer:$nettyVersion")
+        // ... otras librerías de Netty si el escaneo las sigue reportando
     }
 }
 
