b7ba045225c02af1fb157a97f349f2271ec29bbd
jordan Vicaña <jvicanaa@gmail.com>
2025-12-14 16:13:44 -0500

    Update Jenkins pipeline

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,12 @@ plugins {
     java
     id("org.springframework.boot") version "3.5.7"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.owasp.dependencycheck") version "12.1.6"
 }
 
+allprojects {
+    apply(plugin = "org.owasp.dependencycheck")
+}
 
 group = "pe.gob.vuce.cp.sp.sample.experience"
 version = "1.0.0"
@@ -22,48 +26,69 @@ configurations {
 
 repositories {
     mavenCentral()
+
 }
+
+extra["springCloudVersion"] = "2025.0.0"
 extra["netty.version"] = "4.1.128.Final"
+val projectreactorNettyVersion = "1.2.11"
+
+dependencies {
+
+
+    implementation("org.springframework.boot:spring-boot-starter-webflux"){
+//        exclude(group = "io.netty")
+        /*        exclude(group = "io.projectreactor.netty", module = "reactor-netty-http")
+                exclude(group = "io.projectreactor.netty", module = "reactor-netty-core")*/
+    }
+
+    implementation("org.springframework.cloud:spring-cloud-gateway-server-webflux"){
+        exclude(group = "io.projectreactor.netty", module = "reactor-netty-core")
+        exclude(group = "io.projectreactor.netty", module = "reactor-netty-http")
+    }
+
+    implementation("org.springframework.security:spring-security-config"){
+        exclude(group = "org.springframework.security", module = "spring-security-core")
+    }
+    runtimeOnly("org.springframework.cloud:spring-cloud-starter-loadbalancer"){
+        exclude(group = "io.projectreactor.netty", module = "reactor-netty-core")
+        exclude(group = "io.projectreactor.netty", module = "reactor-netty-http")
+        exclude(group = "io.netty")
+
+        exclude(group = "org.springframework.security", module = "spring-security-crypto")
+    }
+
+}
 
 dependencyManagement {
     imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
         mavenBom("io.netty:netty-bom:${property("netty.version")}")
     }
+    /*    dependencies {
+            dependency("io.projectreactor.netty:reactor-netty-http:${projectreactorNettyVersion}")
+            dependency("io.projectreactor.netty:reactor-netty-core:${projectreactorNettyVersion}")
+        }*/
 }
 
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-
-    // Forzar inclusión explícita de Reactor Netty en una versión parcheada para evitar JARs vulnerables anidados
-    implementation("io.projectreactor.netty:reactor-netty-core:1.2.12")
-    implementation("io.projectreactor.netty:reactor-netty-http:1.2.12")
-}
-
-configurations.all {
-    resolutionStrategy {
-        // **IMPORTANTE:** Eliminar cualquier 'force' anterior para reactor-netty-http
-        // y asegurar que los componentes base de Netty se fuerzan a la versión del BOM.
-
-        val nettyVersion = property("netty.version") as String
-
-        // 3. Estrategia de Forzado: Forzar explícitamente los componentes de Netty
-        // a la versión definida, lo que anulará cualquier versión antigua.
-        force("io.netty:netty-codec-http:$nettyVersion") // Corrige CVE-2019-20444 y otros.
-        force("io.netty:netty-handler:$nettyVersion")
-        force("io.netty:netty-buffer:$nettyVersion")
-        force("io.netty:netty-common:$nettyVersion")
-        force("io.netty:netty-transport:$nettyVersion")
-        // Agregar cualquier otro artefacto 'io.netty' que el escaneo siga reportando.
-
-        // --- Forzar Reactor Netty a una versión parcheada compatible ---
-        // Aseguramos que reactor-netty no arrastre versiones antiguas de Netty en artefactos anidados.
-        // Asumimos que la serie 1.2.x es compatible con Spring Boot 3.x; si tu BOM interna requiere
-        // otra versión, ajusta aquí. (Asunción: usamos 1.2.12 para fijar el parche.)
-        force("io.projectreactor.netty:reactor-netty-core:1.2.12")
-        force("io.projectreactor.netty:reactor-netty-http:1.2.12")
-    }
-}
 
 tasks.getByName<Jar>("jar") {
     enabled = false
+}
+
+/*configurations.all {
+    resolutionStrategy {
+        force(
+            "io.netty:netty-transport:${property("netty.version")}",
+            "io.netty:netty-codec-http:${property("netty.version")}",
+            "io.netty:netty-handler:${property("netty.version")}",
+            "io.netty:netty-resolver-dns:${property("netty.version")}"
+        )
+    }
+}*/
+dependencyCheck {
+    format = org.owasp.dependencycheck.reporting.ReportGenerator.Format.ALL.toString()
+    nvd {
+        apiKey = System.getenv("NVD_API_KEY") ?: project.findProperty("nvd.api.key") as String? ?: ""
+    }
 }
